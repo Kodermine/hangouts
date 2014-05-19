e39f16604c53ee8a14ffc1ccc80ff970ab70abb8
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-05-19 11:27:32 +0300

    Create DAO.java

---
+++ JavaHangout1/DAO.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Scanner;
+
+class DAO {
+
+	//DAO = Data Access Object, just a dataholder class
+	
+	//static variables and methods can be accessed without initialization
+	
+	public static Scanner scanner;//scanner (for input)
+	
+	public static List<Question> questions;//list to store each question class (which includes the info about the question)
+	
+	public static int points = 0;//score
+	
+}
